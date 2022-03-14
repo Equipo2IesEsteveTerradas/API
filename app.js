db537fd7a33ef6c86615fe33656c069a48cafbad
Iwo <iwoskater3@hotmail.com>
2022-03-14 16:52:54 +0100

    dburi

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const uri = process.env.DATABSE_URL 
 
 app.get('/', (req, res) => {
+  console.log(uri)
   res.send('Hello World!')
+
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  console.log('spec2')
+  console.log('spec3')
 })
