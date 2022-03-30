9f4709f0a13cd6b6437d46cc0b41425a1a8288ae
Iwo <iwoskater3@hotmail.com>
2022-03-30 14:56:56 +0200

    Spect16 & 19 & 21

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ require('dotenv').config();
 const db = require('./database');
 const CourseModel = require('./models/course');
 const UserModel = require('./models/user');
+const PinModel = require('./models/pin');
 const crypto = require('crypto');
+const { application } = require('express');
 
 
 // Crear el servidor express
 const app = express();
+
 
 
 
@@ -236,6 +239,7 @@ app.get('/api/get_course_details', (req, res)=>{
 })
 
 // ---------------ENDPOINT EXPORT_DATABASE----------------
+// TODO comprobar user y password en lugar del token.
 app.get('/api/export_database', (req, res)=>{
     UserModel.findOne({token: session_token}, (err, user)=>{
         if(err){
@@ -306,6 +310,257 @@ app.get('/api/export_database666', (req, res)=>{
 })
 
 
+function generatePin(){
+    let pin = Math.floor(Math.random() * 9999).toString().padStart(4, '0');
+    return pin;
+}
+async function checkUserByToken(session_token) {
+    return validUser = new Promise((resolve, reject) => {
+        UserModel.findOne({token: session_token}, (err, user)=>{
+            if(err){ 
+                reject(err)
+            }else if(!user) {
+                reject(false)
+            }else if(user) {
+                resolve(true)
+            }
+        })
+    })
+}
+async function getUserByToken(session_token) {
+    if(!session_token){
+        console.log('No existe token de sesion')
+        return false
+    }
+    return new Promise((resolve, reject) => {
+        UserModel.findOne({token: session_token}, (err, user)=>{
+            if(err){
+                reject(err)
+            }else if(!user) {
+                resolve(null)
+            }else if(user) {
+                resolve(user)
+            }
+        })
+    }) 
+}
+
+async function pinExists(actualPin){
+    return new Promise((resolve, reject) => {
+        PinModel.findOne({pinNumber: actualPin}, (err, pinResult)=>{
+            if(err){
+                reject(err)
+            }else if(!pinResult) {
+                console.log('entra en !pinResult')
+                resolve(false)
+            }else if(pinResult){
+                console.log('entra en true pinResult')
+                resolve(true)
+            }
+        }) 
+    })
+}
+// TODO pin_request
+app.get('/api/pin_request', async function(req, res) {
+    var user = await getUserByToken(session_token)
+    var generatedPin = generatePin()
+    var vrTaskId = req.query.vrTaskId || 753
+    var newPin = new PinModel({})
+    if(!user){
+        res.status(200).json({
+            status: "ERROR",
+            message: 'No se encuentra usuario'
+        })
+        return
+    } else if(user){
+        newPin.userId = user.id
+        newPin.exerciseId = vrTaskId
+        console.log(newPin)
+    }
+    while(await pinExists(generatedPin)){
+        console.log('pin existe')
+        generatedPin = generatePin()
+    }
+    if(!await pinExists(generatedPin)){
+        console.log('pin no existe')
+        newPin.pinNumber = generatedPin
+        console.log(newPin)
+        await newPin.save((err, pin)=>{
+            if(err){
+                res.status(200).json({
+                    status: "ERROR",
+                    message: 'Error al guardar pin'
+                })
+            } else if(pin){
+                res.status(200).json({
+                    status: "OK",
+                    message: 'Pin generado correctamente',
+                    pin: pin.pinNumber
+                })
+            }
+        })
+    }
+})
+
+async function getUserById(userId){
+    return new Promise((resolve, reject) => {
+        UserModel.findOne({id: userId},(err,user) => {
+            if(err){
+                reject(err)
+            }else if(!user){
+                resolve(false)
+            }else if(user){
+                resolve(user)
+            }
+        }) 
+    })
+}
+
+async function getTaskByPin(pinInput){
+    return new Promise((resolve, reject) => {
+        PinModel.findOne({pinNumber: pinInput}, (err, pinResult)=>{
+            if(err){
+                reject(err)
+            }else if(!pinResult) {
+                resolve(false)
+            }else if(pinResult){
+                resolve(pinResult.exerciseId)
+            }
+        })
+    })
+}
+
+async function getEntryByPin(pinInput){
+    return new Promise((resolve,reject) => {
+        PinModel.findOne({pinNumber: pinInput}, (err, pinResult)=>{
+            if(err){
+                reject(err)
+            }else if(!pinResult) {
+                resolve(false)
+            }else if(pinResult){
+                resolve(pinResult)
+            }
+        })
+    })
+}
+
+// TODO start_vr_exercise
+app.get('/api/start_vr_exercise', async function(req, res) {
+    // var pinInput = req.query.pin 
+    var pinInput = parseInt(req.body.PIN) 
+    var entry = await getEntryByPin(pinInput)
+    var username;
+    var vrTaskId;
+    if(entry === false){
+        res.status(404).json({
+            status: "ERROR",
+            message: 'Pin incorrecto'
+        })
+        return
+    } else if(entry){
+        user = await getUserById(entry.userId);
+        if(!user){
+            res.status(404).json({
+                status: "ERROR",
+                message: "No se ha podido recuperar al usuario asociado al PIN"
+            })
+            return
+        }
+        res.status(200).json({
+            status: "OK",
+            message: 'Exercise started',
+            username: user.name,
+            VRexerciseID: entry.exerciseId
+        })
+    }
+})
+
+async function getVrTaskById(vrTaskId){
+    return new Promise((resolve, reject) => {
+        CourseModel.findOne({"vr_tasks.ID":vrTaskId}, (err, task) => {
+            
+            if(err){
+                reject(err)
+            }else if(!task){
+                resolve('not found')
+            }else if(task){
+                resolve(task)
+            }
+        }).select("vr_tasks.$").where({"vr_tasks.ID": vrTaskId})
+    })
+}
+
+
+
+// TODO finish_vr_exercise
+app.post('/api/finish_vr_exercise', async function(req, res) {
+    var student;
+    var inputPin = parseInt(req.body.PIN)
+    var autograde = {
+        passed_items: parseInt(req.body.autograde.passed_items),
+        failed_items: parseInt(req.body.autograde.failed_items),
+        score: parseInt(req.body.autograde.score),
+        comments: "...to be decided"
+    }
+    var VRexerciseID = parseInt(req.body.VRexerciseID)
+    var exerciseVersionID = parseInt(req.body.exerciseVersion)
+    var performance_data = {
+        VRexID: parseInt(VRexerciseID),
+        exerciseVersion: parseInt(exerciseVersionID),
+        position_data: {TODO: "complete"}
+    }
+    var entry = await getEntryByPin(inputPin)
+    if(entry===false){
+        return
+    }else if(entry){
+        student = entry.userId;
+    }
+    var completion = {
+        studentID: student,
+        position_data: performance_data.position_data,
+        autograde: autograde,
+        grade: 9,
+        feedback: "Sin querer has curado una trombosis. Buen trabajo"
+    }
+    
+
+    CourseModel.updateOne({"vr_tasks.ID":entry.exerciseId},{$push: {"vr_tasks.$.completions": completion}}, (err, updated) => {
+        if(err){
+            res.status(500).send({
+                status: "ERROR",
+                message: "Ha ocurrido un error en la peticion"
+            })
+        }else if(updated){
+            res.status(200).send({
+                status: 'OK',
+                message: "Los datos del ejercicio se han guardado correctamente"
+            })
+        }
+    })
+            
+
+    
+
+    // var task = await getVrTaskById(5);
+    // console.log(task.vr_tasks[0])
+    // if (task.vr_tasks.length === 1){
+    //     await CourseModel.updateOne({"vr_tasks.ID":entry.exerciseId},{$push: {"vr_tasks.$.completions": completion}}, (err, updated) => {
+    //         if(err){
+    //             res.status(500).send({
+    //                 status: "ERROR",
+    //                 message: "Ha ocurrido un error en la peticion"
+    //             })
+    //         }else if(updated){
+    //             res.status(200).send({
+    //                 status: 'OK',
+    //                 message: "Los datos del ejercicio se han guardado correctamente"
+    //             })
+    //         }
+    //     })
+    // }
+
+})
+
 // -------CONNECTION TEST FUNCTION-------------
 const dbConnection = async() => {
     try{
@@ -321,8 +576,6 @@ const dbConnection = async() => {
 }
 
 
-
-
 //  method to generate a random token
 function generateToken(username, password) {
     var random = Math.random().toString(36).substring(7);
@@ -330,19 +583,6 @@ function generateToken(username, password) {
     createdHash = crypto.createHash('sha256').update(username + password + random).digest('hex').substring(32);
     // console.log(createdHash);
     return createdHash;
-}
-
-// METHOD TO GET USER FROM DB BY IT'S TOKEN
-function getUserByToken(UserModel, token){
-    UserModel.findOne({token: token}, (err, user)=>{
-        if(err){
-            console.log('Error durante la query')
-        } else if (!user){
-            console.log('no se ha encontrado el usuario')
-        } else if(user){
-            return user
-        }
-    })
 }
 
 dbConnection();
