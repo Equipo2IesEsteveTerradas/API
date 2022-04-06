019e135faddaa2262d2980336f0414821d7ae1c5
Iwo <iwoskater3@hotmail.com>
2022-04-06 16:02:25 +0200

    cors fix 3

--- server.js
+++ server.js
@@ -39,11 +39,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // needed code 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 //GET
 router.get('/', (req,res) => {
